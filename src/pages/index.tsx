2046dcca56f995f8650849031130a2d25cc7bc72
mu_tomoya <muton.tm.714@gmail.com>
2023-03-30 18:39:49 +0900

    タイトル変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>プリキュアデータベース</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
