c8b53e611310dfff596819463cce7eb13ab22f5b
mu_tomoya <muton.tm.714@gmail.com>
2023-03-30 22:11:00 +0900

    エフェクトの追加

--- src/pages/precure.tsx
+++ src/pages/precure.tsx
@@ -1,12 +1,15 @@
-import { Age, Color, Precure, Series } from "@/gql/graphql";
+import { Age, Precure, Series } from "@/gql/graphql";
 import { useQuery, gql, DocumentNode } from "@apollo/client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import RadioButton from "@/components/RadioButton";
 import Hero from "@/components/Hero";
 import "flatpickr/dist/flatpickr.min.css";
 import { Japanese } from "flatpickr/dist/l10n/ja.js";
 import Flatpickr from "react-flatpickr";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { GetWindowSize } from "@/components/hooks/GetWindowSize";
 
 const QUERY: DocumentNode = gql`
   query Precure($color: Color, $after: String, $before: String, $age: Age, $series_id: String) {
@@ -21,7 +24,6 @@ const QUERY: DocumentNode = gql`
     }
   }
 `;
-
 export default function PrecurePage(): JSX.Element | null {
   const [color, setColor] = useState<string>();
   const [after, setAfter] = useState<Date>();
@@ -29,7 +31,12 @@ export default function PrecurePage(): JSX.Element | null {
   const [age, setAge] = useState<Age>();
   const [seriesId, setSeriesId] = useState("");
   const [enquiry, setEnquiry] = useState("");
-  const [array, setArray] = useState([]);
+  const [array, setArray] = useState<Series[]>([]);
+  const { width } = GetWindowSize();
+  const imgWidth = width <= 768 ? "10rem" : "300px";
+  const imgHeight = width >= 768 ? "210px" : "400px";
+
+  const div = useRef<HTMLDivElement>(null);
   const option = {
     locale: Japanese,
     dateFormat: "Y/m/d",
@@ -47,11 +54,27 @@ export default function PrecurePage(): JSX.Element | null {
     variables = { before: before };
   }
   const { data, loading, error } = useQuery(QUERY, { variables: variables });
+
   useEffect(() => {
     if (!loading) {
       setArray(data.seriesAll);
+      setAnimation();
     }
   }, [loading]);
+
+  const setAnimation = () => {
+    gsap.fromTo(
+      "#precure",
+      { opacity: 0, y: 10 },
+      {
+        //toの設定
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        stagger: 0.1,
+      }
+    );
+  };
   if (error) {
     console.error(error);
     return null;
@@ -59,7 +82,7 @@ export default function PrecurePage(): JSX.Element | null {
   return (
     <>
       <Hero>プリキュアを検索</Hero>
-      <div className="my-8">
+      <div id="top" className="my-8">
         <ul className="grid grid-cols-4 my-3 mx-1">
           <li>
             <RadioButton str="シリーズ" value="series" name="precure" setValue={setEnquiry} />
@@ -75,8 +98,13 @@ export default function PrecurePage(): JSX.Element | null {
           </li>
         </ul>
         {enquiry === "series" && (
-          <select className="select w-3/4" onChange={(e) => setSeriesId(e.target.value)}>
-            {array.map((item: Series) => (
+          <select
+            className="select w-3/4"
+            onChange={(e) => {
+              setSeriesId(e.target.value);
+            }}
+          >
+            {array.map((item: { id: Series["id"]; title: Series["title"] }) => (
               <option key={item["id"]} value={item["id"]}>
                 {item["title"]}
               </option>
@@ -123,14 +151,18 @@ export default function PrecurePage(): JSX.Element | null {
           <div className="grid grid-cols-2 my-4 md:grid-cols-4 justify-items-center gap-3">
             {data.precureAllStars.map((value: Precure) => (
               <Link href={`/precure/${value["id"]}`} key={value["id"]}>
-                <img
-                  className="w-40 md:w-[300px] rounded-md"
-                  loading="lazy"
-                  src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/precure/${value["cure_name"]}.webp`}
-                  alt={`${value["cure_name"]}`}
-                />
-                <h2 className="mt-1 font-bold text-xs md:text-lg">{value["cure_name"]}</h2>
-                <h3 className="text-xs md:text-base">{value["series"]}</h3>
+                <div ref={div} id="precure">
+                  <img
+                    className="w-40 md:w-[300px] rounded-md"
+                    loading="lazy"
+                    width={imgWidth}
+                    height={imgHeight}
+                    src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/precure/${value["cure_name"]}.webp`}
+                    alt={`${value["cure_name"]}`}
+                  />
+                  <h2 className="mt-1 font-bold text-xs md:text-lg">{value["cure_name"]}</h2>
+                  <h3 className="text-xs md:text-base">{value["series"]}</h3>
+                </div>
               </Link>
             ))}
           </div>
