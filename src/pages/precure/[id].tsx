891b6087562860002f5762dc366a4e132217f015
mu_tomoya <muton.tm.714@gmail.com>
2023-04-01 21:13:00 +0900

    OGP

--- src/pages/precure/[id].tsx
+++ src/pages/precure/[id].tsx
@@ -13,14 +13,9 @@ const Character = ({ precure }: { precure: Precure }) => {
     <div>
       <Head>
         <title>{precure["cure_name"]} | プリキュアデータベース</title>
-        <meta property="og:url" content="https://precure-database.vercel.app" />
-        <meta property="og:type" content="website" />
         <meta property="og:title" content={`${precure["cure_name"]}|プリキュアデータベース`} />
-        <meta property="og:site_name" content="プリキュアデータベース" />
         <meta property="og:image" content={`${process.env.NEXT_PUBLIC_IMAGE_URL}/precure/${precure["cure_name"]}.webp`} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mu_tomoya" />
-        <meta name="twitter:image" content="https://precure-database.vercel.app/ogp.png" />
+        <meta name="description" content={`${precure["cure_name"]}の紹介`} />
       </Head>
       <Hero>キャラクター</Hero>
       <div className="mx-4 my-8">
